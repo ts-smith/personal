4250e50beb6ca030a01518746425fa3eff6ffcc8
Trevor <trevorssmith1392@gmail.com>
2013-03-28 20:22:51 -0700

    fix git

--- Handler/SP.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TupleSections, OverloadedStrings #-}
-module Handler.SP where
-
-import Import
-
-getStaticOneR :: Handler RepHtml
-getStaticOneR = do
-   defaultLayout $ do
-      $(widgetFile "staticone")
